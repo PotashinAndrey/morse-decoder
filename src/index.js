867f1717756f7a397541c7fc37588c96e1da5820
Андрей Поташин <PotashinAndrey@users.noreply.github.com>
2019-09-23 12:43:57 +0500

    feat: add task

--- src/index.js
+++ src/index.js
@@ -37,10 +37,76 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+
 function decode(expr) {
-    // write your solution here
+    const letters = stringToLetters(expr);
+    const dotsAndDashes = binaryToDotDash(letters);
+    const decodedLetter = letterDecoding(dotsAndDashes);
+
+    return decodedLetter;
 }
 
-module.exports = {
-    decode
+function stringToLetters(expr) {
+    const letters = [];
+
+    for (let i = 0; i < expr.length; i += 10) {
+        letters.push(expr.substr(i, 10));
+    }
+
+    return letters;
 }
+
+function binaryToDotDash(letters) {
+    const dotAndDash = [];
+
+    for (let i = 0; i < letters.length; i++) {
+        dotAndDash.push(codeToSymbol(letters[i]));
+    }
+
+    return dotAndDash;
+}
+
+function codeToSymbol(letter) {
+    let symbol = '';
+
+    symb: for (let i = 0; i < letter.length; i+=2) {
+        const temp = "" + letter[i] + letter[i+1];
+
+        switch (temp) {
+            case "00":
+                break;
+
+            case "10":
+                symbol += ".";
+                break;
+
+            case "11":
+                symbol += "-";
+                break;
+
+            default:
+                symbol += " * ";
+                break symb;
+        }
+    }
+
+    return symbol;
+}
+
+function letterDecoding(dotsAndDashes) {
+    let word = '';
+
+    for (let i = 0; i < dotsAndDashes.length; i++) {
+        if (dotsAndDashes[i] == " * ") {
+            word += " ";
+        } else {
+            word += MORSE_TABLE[dotsAndDashes[i]];
+        }
+    }
+
+    return word;
+    }
+
+    module.exports = {
+    decode
+    }
